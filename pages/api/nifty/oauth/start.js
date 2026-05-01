f8596367dd04d6cdbe4355b88a39dc4dd0f42695
Ekaterina <katya.kortiy@gmail.com>
2026-04-30 21:46:13 -0400

    fix: remove invalid scope from OAuth start

--- pages/api/nifty/oauth/start.js
+++ pages/api/nifty/oauth/start.js
@@ -5,7 +5,6 @@ export default function handler(req, res) {
     client_id: process.env.NIFTY_CLIENT_ID,
     redirect_uri: process.env.NIFTY_REDIRECT_URI,
     response_type: 'code',
-    scope: 'project_management',
   });
   res.redirect(`${NIFTY_AUTH_URL}?${params}`);
 }
